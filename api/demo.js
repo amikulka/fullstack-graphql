a95f19c65a6a567c4c4314c1158fa1ec79c3bd81
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-05-10 14:34:48 -0600

    example of relationships in demo file

--- api/demo.js
+++ api/demo.js
@@ -11,21 +11,24 @@ const typeDefs = gql`
   type User {
     email: String!
     avatar: String
-    friends: [User]!
+    shoes: [Shoe]!
   }
   interface Shoe {
     brand: ShoeType!
     size: Int!
+    user: User!
   }
 
   type Sneaker implements Shoe {
     brand: ShoeType!
     size: Int!
+    user: User!
     sport: String!
   }
   type Boot implements Shoe {
     brand: ShoeType!
     size: Int!
+    user: User!
     hasGrip: Boolean!
   }
   input ShoesInput {
@@ -46,20 +49,24 @@ const typeDefs = gql`
   }
 `
 
+const user = {
+  id: 1,
+  email: 'yoda@masters.com',
+  avatar: 'http://yoda.png',
+  shoes: [],
+}
+const shoes = [
+  { brand: 'NIKE', size: 12, sport: 'basketball', user: 1 },
+  { brand: 'TIMBERLAND', size: 10, hasGrip: true, user: 1 },
+]
+
 const resolvers = {
   Query: {
     shoes(_, { input }) {
-      return [
-        { brand: 'NIKE', size: 12, sport: 'basketball' },
-        { brand: 'TIMBERLAND', size: 10, hasGrip: true },
-      ]
+      return shoes
     },
     me() {
-      return {
-        email: 'yoda@masters.com',
-        avatar: 'http://yoda.png',
-        friends: [],
-      }
+      return user
     },
   },
   Mutation: {
@@ -67,10 +74,25 @@ const resolvers = {
       return input
     },
   },
+  User: {
+    shoes() {
+      return shoes
+    },
+  },
   Shoe: {
     __resolveType(shoe) {
       if (shoe.sport) return 'Sneaker'
       return 'Boot'
+    },
+  },
+  Sneaker: {
+    user(shoe) {
+      return user
+    },
+  },
+  Boot: {
+    user(shoe) {
+      return user
     },
   },
 }
